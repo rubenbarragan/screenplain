8dd0360f3a90e33a4a0f111530ff9a943735e632
Ruben Barragan <rbarragan@dropbox.com>
2021-10-08 13:49:48 -0700

    add support for choosing input format

--- screenplain/main.py
+++ screenplain/main.py
@@ -14,13 +14,17 @@ output_formats = (
     'fdx', 'html', 'pdf'
 )
 
+input_formats = (
+    'fdx', 'fountain'
+)
+
 usage = """Usage: %prog [options] [input-file [output-file]]
 
 If a file name parameter is missing or a dash (-), input will be read
 from standard input and output will be written to standard output.
 
 Screenplain will try to auto-detect the output format if
-an output-file is given. Otherwise use the --format option."""
+an output-file is given. Otherwise use the --output-format option."""
 
 
 def invalid_format(parser, message):
@@ -33,11 +37,19 @@ def invalid_format(parser, message):
 def main(args):
     parser = OptionParser(usage=usage)
     parser.add_option(
-        '-f', '--format', dest='output_format',
+        '-o', '--output-format', dest='output_format',
         metavar='FORMAT',
         help=(
             'Set what kind of file to create. FORMAT can be one of ' +
             ', '.join(output_formats)
+        )
+    )
+    parser.add_option(
+        '-i', '--input-format', dest='input_format',
+        metavar='FORMAT',
+        help=(
+            'Set what kind of file to read. FORMAT can be one of ' +
+            ', '.join(input_formats)
         )
     )
     parser.add_option(
@@ -72,23 +84,40 @@ def main(args):
     input_file = (len(args) > 0 and args[0] != '-') and args[0] or None
     output_file = (len(args) > 1 and args[1] != '-') and args[1] or None
 
-    format = options.output_format
-    if format is None and output_file:
+    input_format = options.input_format
+    if  input_format is None and input_file:
+        if input_file.endswith('.fdx'):
+            input_format = 'fdx'
+        elif input_file.endswith('.fountain'):
+            input_format = 'fountain'
+        else:
+            invalid_format(
+                parser,
+                'Could not detect input format from file name ' + input_format
+            )
+    
+    if input_format not in input_formats:
+        invalid_format(
+            parser, 'Unsupported input format: "%s".' % input_format
+        )
+
+    output_format = options.output_format
+    if output_format is None and output_file:
         if output_file.endswith('.fdx'):
-            format = 'fdx'
+            output_format = 'fdx'
         elif output_file.endswith('.html'):
-            format = 'html'
+            output_format = 'html'
         elif output_file.endswith('.pdf'):
-            format = 'pdf'
+            output_format = 'pdf'
         else:
             invalid_format(
                 parser,
                 'Could not detect output format from file name ' + output_file
             )
 
-    if format not in output_formats:
+    if output_format not in output_formats:
         invalid_format(
-            parser, 'Unsupported output format: "%s".' % format
+            parser, 'Unsupported output format: "%s".' % output_format
         )
 
     if input_file:
@@ -96,10 +125,13 @@ def main(args):
         input = codecs.open(input_file, 'rb')
     else:
         input = codecs.getreader('utf-8')(sys.stdin.buffer)
-    # TODO: give the option for the input's format.
-    screenplay = fdx.parse(input)
+    
+    if input_format == 'fdx':
+        screenplay = fdx.parse(input)
+    else:
+        screenplay = fountain.parse(input)
 
-    if format == 'pdf':
+    if output_format == 'pdf':
         output_encoding = None
     else:
         output_encoding = 'utf-8'
@@ -116,19 +148,19 @@ def main(args):
             output = sys.stdout.buffer
 
     try:
-        if format == 'text':
+        if output_format == 'text':
             from screenplain.export.text import to_text
             to_text(screenplay, output)
-        elif format == 'fdx':
+        elif output_format == 'fdx':
             from screenplain.export.fdx import to_fdx
             to_fdx(screenplay, output)
-        elif format == 'html':
+        elif output_format == 'html':
             from screenplain.export.html import convert
             convert(
                 screenplay, output,
                 css_file=options.css, bare=options.bare
             )
-        elif format == 'pdf':
+        elif output_format == 'pdf':
             from screenplain.export.pdf import to_pdf
             to_pdf(screenplay, output, is_strong=options.strong)
     finally:
